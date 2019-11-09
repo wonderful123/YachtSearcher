361cc7e4fda2725c09080dcdffdf19bfffcf5374
Joshua Jenkins <clearharry@gmail.com>
2019-11-09 15:33:46 +1000

    change rooturl

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'frontend',
     environment,
-    rootURL: '/',
+    rootURL: '/YachtSearcher/frontend/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
