22841347ff803f6b090f2e14187be015c593b456
Joshua Jenkins <clearharry@gmail.com>
2019-11-09 17:38:03 +1000

    Deployment

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-  host: 'http://http://108.161.143.35/:3000',
+  host: 'http://http://108.161.143.35:3000',
 });
--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'frontend',
     environment,
-    rootURL: '/YachtSearcher/frontend/dist/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
