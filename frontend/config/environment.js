38b4c1f4d7b1a6560fdb6517e05852eefc9c335f
Joshua Jenkins <clearharry@gmail.com>
2019-11-10 11:50:03 +1000

    Added route links to header

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'frontend',
     environment,
-    rootURL: '/YachtSearcher/frontend/dist/',
+    rootURL: '/test/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
