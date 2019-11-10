20851861c91e9fb79d6129b76c9efe5e4eeb67fb
Joshua Jenkins <clearharry@gmail.com>
2019-11-10 12:00:14 +1000

    rooturl change

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'frontend',
     environment,
-    rootURL: '/test/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
