7e6e64bed00986b79d66df74002ac8646a126511
Joshua Jenkins <clearharry@gmail.com>
2019-11-15 10:42:07 +1000

    Added query params

--- frontend/app/routes/boats.js
+++ frontend/app/routes/boats.js
@@ -1,7 +1,18 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({
-  model() {
-    return this.store.query('boat', { per_page: 10 });
+export default
+class extends Route {
+  queryParams = {
+    per_page: { refreshModel: true },
+    page: { refreshModel: true },
+    search: { refreshModel: true },
+    sortby: { refreshModel: true },
+    length: { refreshModel: true },
+    price: { refreshModel: true },
+    year: { refreshModel: true },
   }
-});
+
+  model(params) {
+    return this.store.query('boat', params);
+  }
+}
