f02695184936ce595250fe9b398861038b37e9e1
Joshua Jenkins <clearharry@gmail.com>
2019-11-15 10:39:57 +1000

    Include price symbol

--- frontend/app/components/price-text.js
+++ frontend/app/components/price-text.js
@@ -5,7 +5,8 @@ export default Component.extend({
   tagName: 'span',
   priceText: computed('record', function() {
     if (this.record.price) {
-      return this.record.priceSymbol + this.record.price.toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+      const symbol = this.record.priceSymbol.replace('US', ''); // Assume USD is default but should A$, etc.
+      return symbol + this.record.price.toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
     }
   })
 });
