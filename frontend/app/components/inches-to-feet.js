cd9e1cda6ebe9f49df38cc3c556f6fc9344b1ea8
Joshua Jenkins <clearharry@gmail.com>
2019-11-17 20:19:06 +1000

    Bug fix

--- frontend/app/components/inches-to-feet.js
+++ frontend/app/components/inches-to-feet.js
@@ -4,10 +4,12 @@ import { computed } from '@ember/object';
 export default Component.extend({
   tagName: 'span',
   length: computed('inches', function() {
-    const totalInches = this.inches || this.record.lengthInches;
-    const feet = Math.floor(totalInches/12);
-    const inches = totalInches % 12;
-    const inchText = (inches === 0) ? `` : ` ${inches}"`;
-    return `${feet}'${inchText}`;
+    if (this.inches || (this.record && this.record.lengthInches)) {
+      const totalInches = this.inches || this.record.lengthInches;
+      const feet = Math.floor(totalInches/12);
+      const inches = totalInches % 12;
+      const inchText = (inches === 0) ? `` : ` ${inches}"`;
+      return `${feet}'${inchText}`;
+    }
   })
 });
