81d698887e7f8d1f8ce644d91b965a33de156853
Joshua Jenkins <clearharry@gmail.com>
2019-11-18 09:00:35 +1000

    Finalizing changes

--- frontend/app/controllers/boats.js
+++ frontend/app/controllers/boats.js
@@ -1,24 +1,36 @@
 import Controller from '@ember/controller';
-import { action, get, set } from '@ember/object';
+import { action, set, computed } from '@ember/object';
 
 export default
 class BoatsController extends Controller {
   queryParams = ['page', 'per_page', 'search', 'sortby', 'length', 'price', 'year'];
   page = 1;
-  per_page = 10;
+  per_page = 20;
   search = '';
-  sortby = '';
+  sortby = 'first-found_desc'
   year = '';
   price = '';
   length = '';
 
-  @action
-  displayChangeAction(d) {
-    this.setProperties({
-      page: get(d, 'currentPageNumber'),
-      per_page: get(d, 'pageSize'),
-    });
-    console.log('D', d)
+  // These are used to set inital component states, eg. Filter
+  @computed
+  get urlQueryParams() {
+    return {
+      search: this.search,
+      year: this.year,
+      price: this.price,
+      length: this.length
+    }
+  }
+
+  @computed
+  get urlSortParam() {
+    return this.sortby;
+  }
+
+  @computed('model.[]')
+  get boatsFound() {
+    return this.model.meta.count > 0 ? true : false;
   }
 
   @action
@@ -26,6 +38,13 @@ class BoatsController extends Controller {
     set(this, 'year', values.year);
     set(this, 'price', values.price);
     set(this, 'length', values.length);
+    set(this, 'search', values.search);
+    set(this, 'page', 1); // Return to first page
+  }
+
+  @action
+  doSort(property, direction) {
+    set(this, 'sortby', `${property}_${direction}`);
     set(this, 'page', 1); // Return to first page
   }
 }
