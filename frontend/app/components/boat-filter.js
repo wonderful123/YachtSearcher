f6e338f1d24579a911fc1e86b2716897f1ab2e18
Joshua Jenkins <clearharry@gmail.com>
2019-11-18 08:59:32 +1000

    Working

--- frontend/app/components/boat-filter.js
+++ frontend/app/components/boat-filter.js
@@ -1,29 +1,61 @@
 import Component from '@ember/component';
-import { observer } from '@ember/object';
+import { action, set } from '@ember/object';
 
-export default Component.extend({
-  classNames: 'boat-filter',
+export default
+class extends Component {
+  classNames = ['boat-filter'];
+  search = '';
+  minPrice = '';
+  maxPrice = '';
+  minYear = '';
+  maxYear = '';
+  minLength = '';
+  maxLength = '';
 
-  init() {
+  didInsertElement() {
     this._super(...arguments);
-    this.filter({});
-    this.inputValues = {};
-  },
 
-  maskMinPrice: observer('minPrice', function() {
-    if (this.minPrice !== '') this.set('minPrice', this.minPrice.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ","));
-    this.set('inputValues.minPrice', this.minPrice.replace(/\D/g, ""));
-  }),
-
-  maskMaxPrice: observer('maxPrice', function() {
-    if (this.maxPrice !== '') this.set('maxPrice', this.maxPrice.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ","));
-    this.set('inputValues.maxPrice', this.maxPrice.replace(/\D/g, ""));
-  }),
-
-  actions: {
-    handleFilter() {
-      let filterAction = this.filter;
-      filterAction(this.inputValues);
-    },
+    const setMinMax = (param) => {
+      const p = param.toLowerCase();
+      if (this.urlQueryParams[p]) {
+        let minMax = this.urlQueryParams[p].split('to');
+        set(this, `min${param}`, minMax[0]);
+        set(this, `max${param}`, minMax[1]);
+      }
+    }
+    setMinMax('Price');
+    setMinMax('Year');
+    setMinMax('Length');
+    // Return length from inches back to feet
+    if (this.minLength) set(this, 'minLength', this.minLength/12);
+    if (this.maxLength) set(this, 'maxLength', this.maxLength/12);
+    if (this.urlQueryParams.search) set(this, 'search', this.urlQueryParams.search);
   }
-});
+
+  // Convert input to values parent action expects
+  @action
+  handleFilter() {
+    const buildQuery = (min, max) => `${min}to${max}` === 'to' ? '' : `${min}to${max}`;
+    const maxInches = this.maxLength === '' ? '' : this.maxLength * 12
+    const minInches = this.minLength === '' ? '' : this.minLength * 12
+    const lengthQuery = buildQuery(minInches, maxInches)
+    const priceQuery = buildQuery(this.minPrice, this.maxPrice)
+    const yearQuery = buildQuery(this.minYear, this.maxYear)
+
+    let filters = {
+      search: this.search,
+      length: lengthQuery,
+      price: priceQuery,
+      year: yearQuery
+    }
+
+    // Call parent action and update query filters
+    this.updateFilters(filters);
+  }
+
+  //
+  // maskMaxPrice = computed('maxPrice', function() {
+  //   if (this.maxPrice !== '') this.set('maxPrice', this.maxPrice.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+  //   this.set('inputValues.maxPrice', this.maxPrice.replace(/\D/g, ""));
+  // })
+}
