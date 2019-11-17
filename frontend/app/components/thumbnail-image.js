5bfb5386c4ed8402d38e20dce5e0bc53e04ead4e
Joshua Jenkins <clearharry@gmail.com>
2019-11-17 20:20:04 +1000

    Can click thumbnail and modal shows with more images loaded from first listing.

--- frontend/app/components/thumbnail-image.js
+++ frontend/app/components/thumbnail-image.js
@@ -1,9 +1,37 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
+import { computed, action } from '@ember/object';
+import { inject } from '@ember/service';
 import config from '../config/environment';
+import ajax from 'ember-ajax';
 
-export default Component.extend({
-  url: computed('record', function() {
+export default
+class ThumbnailImage extends Component {
+  isModalOpen = false;
+  images = [];
+
+  @inject
+  store
+
+  @computed('record')
+  get url() {
     return config.image_server + this.record.thumbnail;
-  })
-});
+  }
+
+  @computed('config.image_server')
+  get imageServer() {
+    return config.image_server;
+  }
+
+  @action
+  toggleModal() {
+    this.toggleProperty('isModalOpen');
+    this.set('imagesLoading', true)
+
+    const url = `${config.rails_host}/boats/${this.record.id}/images`;
+    ajax(url, 'GET').then(response => {
+      const images = response.data.attributes.images;
+      this.set('images', images);
+      this.set('imagesLoading', false);
+    });
+  }
+}
