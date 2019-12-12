ae42d4785a3614cec6dc6cc5b278eef14ee5e34f
Joshua Jenkins <clearharry@gmail.com>
2019-12-12 21:35:41 +1000

    Upgraded ember version

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -23,17 +23,17 @@ module.exports = function(environment) {
     }
   };
 
-  ENV['ember-google-maps'] = {
-    key: process.env.GOOGLE_MAPS_API_KEY, // Using .env files in this example
-    language: 'en',
-    region: 'GB',
-    protocol: 'https',
-    version: '3.35',
-    libraries: ['geometry', 'places'], // Optional libraries
-    // client: undefined,
-    // channel: undefined,
-    // baseUrl: '//maps.googleapis.com/maps/api/js'
-  };
+  // ENV['ember-google-maps'] = {
+  //   key: process.env.GOOGLE_MAPS_API_KEY, // Using .env files in this example
+  //   language: 'en',
+  //   region: 'GB',
+  //   protocol: 'https',
+  //   version: '3.35',
+  //   libraries: ['geometry', 'places'], // Optional libraries
+  //   // client: undefined,
+  //   // channel: undefined,
+  //   // baseUrl: '//maps.googleapis.com/maps/api/js'
+  // };
 
 
   if (environment === 'development') {
