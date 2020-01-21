36db9e909ab3905101eff7eee3f4faeb6a74fe60
Joshua Jenkins <clearharry@gmail.com>
2020-01-21 11:06:10 +1000

    Change to glimmer component

--- frontend/app/components/favourite-list.js
+++ frontend/app/components/favourite-list.js
@@ -1,4 +1,4 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { action, computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 
