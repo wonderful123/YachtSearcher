44b5af1bbbaf9eaf7841244420f51818fc0099ba
Joshua Jenkins <clearharry@gmail.com>
2020-01-31 00:02:42 +1000

    duplicate paste

--- frontend/tests/unit/routes/map-test.js
+++ frontend/tests/unit/routes/map-test.js
@@ -8,14 +8,4 @@ module('Unit | Route | map', function(hooks) {
     let route = this.owner.lookup('route:map');
     assert.ok(route);
   });
-});import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
-
-module('Unit | Route | map', function(hooks) {
-  setupTest(hooks);
-
-  test('it exists', function(assert) {
-    let route = this.owner.lookup('route:map');
-    assert.ok(route);
-  });
 });
