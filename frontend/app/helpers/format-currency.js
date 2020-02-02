3ea7ec50f5871a1dbcb11b1e12a3b3ba8b9d3830
Joshua Jenkins <clearharry@gmail.com>
2020-02-02 21:46:48 +1000

    update if number a string

--- frontend/app/helpers/format-currency.js
+++ frontend/app/helpers/format-currency.js
@@ -3,7 +3,7 @@ import { helper } from '@ember/component/helper';
 export function formatCurrency(params) {
   let [price, symbol] = params;
   symbol = symbol.replace('US', ''); // Assume USD is default but should A$, etc.
-  return symbol + price.toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+  return symbol + parseFloat(price).toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
 }
 
 export default helper(formatCurrency);
