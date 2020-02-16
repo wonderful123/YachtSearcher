fee77066e807d43c94307c6ef3bec05a663f1619
Joshua Jenkins <clearharry@gmail.com>
2020-02-16 10:25:59 +1000

    Simplified filter with DDAU

--- frontend/app/controllers/boats.js
+++ frontend/app/controllers/boats.js
@@ -2,50 +2,98 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from "@glimmer/tracking";
 
+class FilterValue {
+  @tracked value = '';
+  @tracked min = '';
+  @tracked max = '';
+  @tracked type = ''; // search, price, etc
+
+  // Pass either obj with value or min/min properties
+  constructor(type, value) {
+    this.type = type;
+    if (type === 'search') {
+      if (value) this.value = value;
+    } else {
+      if (value[0]) this.min = value[0];
+      if (value[1]) this.max = value[1];
+    }
+  }
+
+  get param() {
+    // Value for single values, otherwise min/max range value
+    if (this.type === 'length') {
+      // convert back to total inches from feet
+      const min = this.min ? this.min * 12 : '';
+      const max = this.max ? this.max * 12 : '';
+      return `${min}to${max}` === 'to' ? '' : `${min}to${max}`;
+    } else if (this.min || this.max) {
+      return `${this.min}to${this.max}` === 'to' ? '' : `${this.min}to${this.max}`;
+    } else {
+      return this.value;
+    }
+  }
+}
+
+class Filter {
+  @tracked search;
+  @tracked year;
+  @tracked price;
+  @tracked length;
+
+  // Convert query string to { min: value, max: value }
+  getMinMax(queryParam) {
+    return queryParam ? queryParam.toLowerCase().split('to') : '';
+  }
+
+  // Build class of from params
+  constructor(search, year, price, length) {
+    this.search = new FilterValue('search', search);
+    this.year = new FilterValue('year', this.getMinMax(year));
+    this.price = new FilterValue('price', this.getMinMax(price));
+    this.length = new FilterValue('length', this.getMinMax(length));
+    if (this.length.min) this.length.min = this.min.length / 12; // Work in feet not total inches
+    if (this.length.max) this.length.max = this.max.length / 12;
+  }
+}
+
 export default
 class BoatsController extends Controller {
   queryParams = ['page', 'per_page', 'search', 'sortby', 'length', 'price', 'year'];
   @tracked page = 1;
   @tracked per_page = 20;
+  @tracked sortby = 'first-found_desc';
   @tracked search = '';
-  @tracked sortby = 'first-found_desc'
   @tracked year = '';
   @tracked price = '';
   @tracked length = '';
+  @tracked isMasked = ''; // for modal masking
 
-  FILTERS = ['search', 'year', 'price', 'length'];
+  @tracked filter = new Filter(this.search, this.year, this.price, this.length);
 
-  // These are used to set inital component states, eg. Filter
-  get urlQueryParams() {
-    return {
-      search: this.search,
-      year: this.year,
-      price: this.price,
-      length: this.length
-    }
+  @action
+  doSort(property, direction) {
+    this.sortby = `${property}_${direction}`;
+    this.page = 1; // Return to first page
   }
 
-  // Return array of filters used
-  get filters() {
-    // Convert query string to { min: value, max: value }
-    const getMinMax = (param) => {
-      const minMax = this[param].toLowerCase().split('to');
-      return { min: minMax[0], max: minMax[1] };
-    }
+  // Used for modal background masking
+  @action
+  maskMainContent(state) {
+    this.isMasked = state;
+  }
 
-    return this.FILTERS.map(f => {
-      if (f === 'search') {
-        return { name: 'search', value: this[f] };
-      } else {
-        let values = getMinMax(f);
-        values.name = f;
-        if (f === 'length') {
-          values.min /= 12;
-          values.max /= 12;
-        }
-        return values;
-      }
-    });
+  // Update filter and query params
+  @action
+  updateFilter(filterType, property, value) {
+    this.filter[filterType][property] = value;
+
+    this.search = this.filter.search.param;
+    this.year = this.filter.year.param;
+    this.length = this.filter.length.param;
+    this.price = this.filter.price.param;
+    this.page = 1; // Return to first page
+
+    this.filter = this.filter;
   }
 
   get urlSortParam() {
@@ -54,20 +102,5 @@ class BoatsController extends Controller {
 
   get boatsFound() {
     return this.model.meta.count > 0 ? true : false;
-  }
-
-  @action
-  updateFilters(values) {
-    this.year = values.year;
-    this.price = values.price;
-    this.length = values.length;
-    this.search = values.search;
-    this.page = 1; // Return to first page
-  }
-
-  @action
-  doSort(property, direction) {
-    this.sortby = `${property}_${direction}`;
-    this.page = 1; // Return to first page
   }
 }
