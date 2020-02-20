d6fa546898816ca76ceda24b04d3d08109a761fa
Joshua Jenkins <clearharry@gmail.com>
2020-02-20 21:19:01 +1000

    Move first found into carousel

--- frontend/app/components/listing/listing.js
+++ frontend/app/components/listing/listing.js
@@ -3,7 +3,17 @@ import Component from '@glimmer/component';
 export default
 class BoatListing extends Component {
   get firstFound() {
-    const m = new Date(this.args.boat.firstFound);
-    return m.getUTCDate() + "/" + + m.getUTCMonth()  + "/" + m.getUTCFullYear();
+    const d = new Date(this.args.boat.firstFound);
+    return d.getUTCDate() + "/" + d.getUTCMonth()  + "/" + d.getUTCFullYear();
+  }
+
+  get date() {
+    const MONTHS = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG',
+                    'SEP', 'OCT', 'NOV', 'DEC'];
+    const d = new Date(this.args.boat.firstFound);
+    return {
+      day: d.getUTCDate(),
+      month: MONTHS[d.getUTCMonth()]
+    };
   }
 }
