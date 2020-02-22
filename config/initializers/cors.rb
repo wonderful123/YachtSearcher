546c56724d133db715f1e33106753928cea3de54
Joshua Jenkins <clearharry@gmail.com>
2020-02-23 09:26:26 +1000

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4200', 'smoothcontrol.com', 'localhost:3000'
+    origins '*'
 
     resource '*',
       headers: :any,
