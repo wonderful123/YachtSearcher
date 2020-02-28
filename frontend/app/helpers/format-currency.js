ab8ea3f8fdd9485cf51d44c659ccf22d78dd0cec
Joshua Jenkins <clearharry@gmail.com>
2020-02-28 23:10:47 +1000

    error checking

--- frontend/app/helpers/format-currency.js
+++ frontend/app/helpers/format-currency.js
@@ -1,9 +1,15 @@
-import { helper } from '@ember/component/helper';
+import {
+  helper
+} from '@ember/component/helper';
 
 export function formatCurrency(params) {
-  let [price, symbol] = params;
-  symbol = symbol.replace('US', ''); // Assume USD is default but should A$, etc.
-  return symbol + parseFloat(price).toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+  if (params) {
+    let [price, symbol] = params;
+    symbol = symbol.replace('US', ''); // Assume USD is default but should A$, etc.
+    return symbol + parseFloat(price).toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+  } else {
+    return '';
+  }
 }
 
 export default helper(formatCurrency);
