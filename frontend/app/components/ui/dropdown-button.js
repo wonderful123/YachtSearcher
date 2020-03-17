13004e72a239ef98630058e914ebcde00406d229
Joshua Jenkins <clearharry@gmail.com>
2020-03-17 19:07:35 +1000

    Internal dropdown calculation that is now optional

--- frontend/app/components/ui/dropdown-button.js
+++ frontend/app/components/ui/dropdown-button.js
@@ -4,16 +4,28 @@ import { tracked } from '@glimmer/tracking';
 
 export default
 class DropdownButton extends Component {
-  @tracked isSelected = false;
+  @tracked _isSelected = false;
+
+  get isSelected() {
+    if (this.args.isSelected != undefined) {
+      return (this._isSelected = this.args.isSelected);
+    }
+
+    return this._isSelected;
+  }
 
   get iconDirection() {
-    return this.isSelected ? 'chevron-up' : 'chevron-down';
+    return this._isSelected ? 'chevron-up' : 'chevron-down';
   }
 
   @action
   onClick() {
-    this.isSelected = !this.isSelected;
-
     this.args.onClick();
+
+    if (this.args.isSelected != undefined) {
+      this._isSelected = this.args.isSelected;
+    } else {
+      this._isSelected = !this._isSelected;
+    }
   }
 }
