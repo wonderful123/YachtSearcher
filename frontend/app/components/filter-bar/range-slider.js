133546618c212bfe2d5f288a2186cfe7750a20f4
Joshua Jenkins <clearharry@gmail.com>
2020-03-17 19:06:11 +1000

    Access to this in range function through @action

--- frontend/app/components/filter-bar/range-slider.js
+++ frontend/app/components/filter-bar/range-slider.js
@@ -1,4 +1,5 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class RangeSliderComponent extends Component {
   min = this.args.rangeMin;
@@ -9,24 +10,25 @@ export default class RangeSliderComponent extends Component {
   }
 
   tooltips = [{
-      to: this.formatter,
-      min: this.min,
-      max: this.max,
-      formatFunction: this.args.rangeFormatter,
-    }, {
-      to: this.formatter,
-      min: this.min,
-      max: this.max,
-      formatFunction: this.args.rangeFormatter,
+    to: this.formatter,
+    min: this.min,
+    max: this.max,
+    formatFunction: this.formatter,
+  }, {
+    to: this.formatter,
+    min: this.min,
+    max: this.max,
+    formatFunction: this.formatter,
   }];
 
+  @action
   formatter(value) {
     if (value === this.max) {
       return 'No max';
     } else if (value === this.min) {
       return 'No min';
     } else {
-      return this.formatFunction(value);
+      return this.args.rangeFormatter(value);
     }
   }
 }
